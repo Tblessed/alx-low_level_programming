064187533df42037d4cdd365044532be7ab94559
Tblessed <mungaitabby@gmail.com>
2023-04-14 18:36:53 -0700

    create file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,25 +8,25 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fild = 0, chk = 0;
-	char *buff;
+	char *buffer;
 
 	if (!filename || !letters)
 		return (0);
 
-	filq = open(filename, O_RDONLY);
-	if (filq < 0)
+	fild = open(filename, O_RDONLY);
+	if (fild < 0)
 		return (0);
 
-	buff = malloc(sizeof(char) * (letters));
-	if (!buff)
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
 		return (0);
 
-	chk = read(filq, buff, letters);
-	chk = write(STDOUT_FILENO, buff, chk);
+	chk = read(fild, buffer, letters);
+	chk = write(STDOUT_FILENO, buffer, chk);
 	if (chk < 0)
 		return (0);
 
-	close(filq);
-	free(buff);
+	close(fild);
+	free(buffer);
 	return (chk);
 }
